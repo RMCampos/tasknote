6ef490c44f513bb530b7711d290b4a66494b9560
Ricardo Campos <ricardompcampos@gmail.com>
2026-04-05 16:20:59 -0300

    chore: finally formatted tf file nicely

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,22 +1,21 @@
 terraform {
   required_providers {
     kubernetes = {
-      source = "hashicorp/kubernetes"
-      version = ">= 2.0.0" 
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.0"
     }
   }
 
   backend "s3" {
-    bucket = "tasknote"
-    key = "kubernetes/terraform.tfstate"
-    region = "auto"
-    endpoints = { s3 = "https://d17eb09b6bce2f90e16e800bb2a6baf9.r2.cloudflarestorage.com" 
-}
+    bucket                      = "tasknote"
+    key                         = "kubernetes/terraform.tfstate"
+    region                      = "auto"
+    endpoints                   = { s3 = "https://d17eb09b6bce2f90e16e800bb2a6baf9.r2.cloudflarestorage.com" }
     skip_credentials_validation = true
-    skip_region_validation = true
-    skip_requesting_account_id = true
-    skip_metadata_api_check = true
-    skip_s3_checksum = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    skip_metadata_api_check     = true
+    skip_s3_checksum            = true
   }
 }
 
@@ -25,47 +24,47 @@ provider "kubernetes" {
 }
 
 variable "db_user" {
-  type = string
+  type      = string
   sensitive = true
 }
 
 variable "db_password" {
-  type = string
+  type      = string
   sensitive = true
 }
 
 variable "db_name" {
-  type = string
+  type      = string
   sensitive = true
 }
 
 variable "security_key" {
-    type = string
-    sensitive = true
+  type      = string
+  sensitive = true
 }
 
 variable "mailgun_apikey" {
-  type = string
+  type      = string
   sensitive = true
 }
 
 variable "cors_allowed_origins" {
-  type = string
+  type    = string
   default = "https://tasknote.darkroasted.vps-kinghost.net"
 }
 
 variable "root_log_level" {
-  type = string
+  type    = string
   default = "INFO"
 }
 
 variable "backend_image" {
-  type = string
+  type    = string
   default = "pull ghcr.io/rmcampos/tasknote/api:15"
 }
 
 variable "frontend_image" {
-  type = string
+  type    = string
   default = "ghcr.io/rmcampos/tasknote/app:app-v2026.03.17.18"
 }
 
@@ -77,22 +76,22 @@ resource "kubernetes_namespace_v1" "tasknote" {
 
 resource "kubernetes_secret_v1" "tasknote_secrets" {
   metadata {
-    name = "tasknote-secrets"
+    name      = "tasknote-secrets"
     namespace = kubernetes_namespace_v1.tasknote.metadata[0].name
   }
 
   data = {
-    postgres_user = var.db_user
+    postgres_user     = var.db_user
     postgres_password = var.db_password
-    postgres_db = var.db_name
-    security_key = var.security_key
-    mailgun_apikey = var.mailgun_apikey
+    postgres_db       = var.db_name
+    security_key      = var.security_key
+    mailgun_apikey    = var.mailgun_apikey
   }
 }
 
 resource "kubernetes_persistent_volume_claim_v1" "tasknote_db_data" {
   metadata {
-    name = "postgres-data-pvc"
+    name      = "postgres-data-pvc"
     namespace = kubernetes_namespace_v1.tasknote.metadata[0].name
   }
   spec {
@@ -107,7 +106,7 @@ resource "kubernetes_persistent_volume_claim_v1" "tasknote_db_data" {
 
 resource "kubernetes_deployment_v1" "tasknote_db" {
   metadata {
-    name = "tasknote-db"
+    name      = "tasknote-db"
     namespace = kubernetes_namespace_v1.tasknote.metadata[0].name
   }
   spec {
@@ -118,9 +117,9 @@ resource "kubernetes_deployment_v1" "tasknote_db" {
       spec {
         container {
           image = "postgres:15.8-bookworm"
-          name = "postgres"
+          name  = "postgres"
           volume_mount {
-            name = "postgres-storage"
+            name       = "postgres-storage"
             mount_path = "/var/lib/postgresql/data"
           }
           env {
@@ -128,7 +127,7 @@ resource "kubernetes_deployment_v1" "tasknote_db" {
             value_from {
               secret_key_ref {
                 name = kubernetes_secret_v1.tasknote_secrets.metadata[0].name
-                key = "postgres_user"
+                key  = "postgres_user"
               }
             }
           }
@@ -137,7 +136,7 @@ resource "kubernetes_deployment_v1" "tasknote_db" {
             value_from {
               secret_key_ref {
                 name = kubernetes_secret_v1.tasknote_secrets.metadata[0].name
-                key = "postgres_password"
+                key  = "postgres_password"
               }
             }
           }
@@ -146,7 +145,7 @@ resource "kubernetes_deployment_v1" "tasknote_db" {
             value_from {
               secret_key_ref {
                 name = kubernetes_secret_v1.tasknote_secrets.metadata[0].name
-                key = "postgres_db"
+                key  = "postgres_db"
               }
             }
           }
@@ -165,7 +164,7 @@ resource "kubernetes_deployment_v1" "tasknote_db" {
 
 resource "kubernetes_service_v1" "tasknote_db_svc" {
   metadata {
-    name = "tasknote-db-svc"
+    name      = "tasknote-db-svc"
     namespace = kubernetes_namespace_v1.tasknote.metadata[0].name
   }
   spec {
@@ -177,7 +176,7 @@ resource "kubernetes_service_v1" "tasknote_db_svc" {
 
 resource "kubernetes_deployment_v1" "tasknote_backend" {
   metadata {
-    name = "tasknote-backend"
+    name      = "tasknote-backend"
     namespace = kubernetes_namespace_v1.tasknote.metadata[0].name
   }
   spec {
@@ -188,18 +187,18 @@ resource "kubernetes_deployment_v1" "tasknote_backend" {
       spec {
         container {
           image = var.backend_image
-          name = "backend"
+          name  = "backend"
           env {
             name = "POSTGRES_DB"
-            value_from { 
+            value_from {
               secret_key_ref {
                 name = kubernetes_secret_v1.tasknote_secrets.metadata[0].name
-                key = "postgres_db"
+                key  = "postgres_db"
               }
             }
           }
           env {
-            name = "POSTGRES_HOST"
+            name  = "POSTGRES_HOST"
             value = "tasknote-db-svc"
           }
           env {
@@ -207,59 +206,59 @@ resource "kubernetes_deployment_v1" "tasknote_backend" {
             value_from {
               secret_key_ref {
                 name = kubernetes_secret_v1.tasknote_secrets.metadata[0].name
-                key = "postgres_user"
+                key  = "postgres_user"
               }
             }
           }
           env {
             name = "POSTGRES_PASSWORD"
-            value_from { 
+            value_from {
               secret_key_ref {
                 name = kubernetes_secret_v1.tasknote_secrets.metadata[0].name
-                key = "postgres_password"
+                key  = "postgres_password"
               }
             }
           }
           env {
-            name = "POSTGRES_PORT"
+            name  = "POSTGRES_PORT"
             value = "5432"
           }
           env {
-            name = "CORS_ALLOWED_ORIGINS"
+            name  = "CORS_ALLOWED_ORIGINS"
             value = var.cors_allowed_origins
           }
           env {
-            name = "SERVER_SERVLET_CONTEXT_PATH"
+            name  = "SERVER_SERVLET_CONTEXT_PATH"
             value = "/"
           }
           env {
-            name = "ROOT_LOG_LEVEL"
+            name  = "ROOT_LOG_LEVEL"
             value = var.root_log_level
           }
           env {
             name = "SECURITY_KEY"
-            value_from { 
+            value_from {
               secret_key_ref {
                 name = kubernetes_secret_v1.tasknote_secrets.metadata[0].name
-                key = "security_key"
+                key  = "security_key"
               }
             }
           }
           env {
-            name = "TARGET_ENV"
+            name  = "TARGET_ENV"
             value = "production"
           }
           env {
             name = "MAILGUN_APIKEY"
-            value_from { 
+            value_from {
               secret_key_ref {
                 name = kubernetes_secret_v1.tasknote_secrets.metadata[0].name
-                key = "mailgun_apikey"
+                key  = "mailgun_apikey"
               }
             }
           }
           resources {
-            limits = { memory = "256Mi", cpu = "500m" }
+            limits   = { memory = "256Mi", cpu = "500m" }
             requests = { memory = "256Mi", cpu = "250m" }
           }
         }
@@ -270,13 +269,13 @@ resource "kubernetes_deployment_v1" "tasknote_backend" {
 
 resource "kubernetes_service_v1" "tasknote_backend_svc" {
   metadata {
-    name = "tasknote-backend-svc"
+    name      = "tasknote-backend-svc"
     namespace = kubernetes_namespace_v1.tasknote.metadata[0].name
   }
   spec {
     selector = { app = "tasknote-backend" }
     port {
-      port = 8585
+      port        = 8585
       target_port = 8585
     }
   }
@@ -284,7 +283,7 @@ resource "kubernetes_service_v1" "tasknote_backend_svc" {
 
 resource "kubernetes_deployment_v1" "tasknote_frontend" {
   metadata {
-    name = "tasknote-frontend"
+    name      = "tasknote-frontend"
     namespace = kubernetes_namespace_v1.tasknote.metadata[0].name
   }
   spec {
@@ -295,10 +294,10 @@ resource "kubernetes_deployment_v1" "tasknote_frontend" {
       spec {
         container {
           image = var.frontend_image
-          name = "frontend"
+          name  = "frontend"
           port { container_port = 5000 }
           env {
-            name = "VITE_BACKEND_SERVER"
+            name  = "VITE_BACKEND_SERVER"
             value = "https://tasknoteapi.darkroasted.vps-kinghost.net"
           }
         }
@@ -309,13 +308,13 @@ resource "kubernetes_deployment_v1" "tasknote_frontend" {
 
 resource "kubernetes_service_v1" "tasknote_frontend_svc" {
   metadata {
-    name = "tasknote-frontend-svc"
+    name      = "tasknote-frontend-svc"
     namespace = kubernetes_namespace_v1.tasknote.metadata[0].name
   }
   spec {
     selector = { app = "tasknote-app" }
     port {
-      port = 5000
+      port        = 5000
       target_port = 5000
     }
     type = "ClusterIP"
@@ -325,7 +324,7 @@ resource "kubernetes_service_v1" "tasknote_frontend_svc" {
 # Unified Ingress for App and API
 resource "kubernetes_ingress_v1" "tasknote_ingress" {
   metadata {
-    name = "tasknote-ingress"
+    name      = "tasknote-ingress"
     namespace = kubernetes_namespace_v1.tasknote.metadata[0].name
     annotations = {
       "kubernetes.io/ingress.class"    = "traefik"
@@ -334,14 +333,14 @@ resource "kubernetes_ingress_v1" "tasknote_ingress" {
   }
   spec {
     tls {
-      hosts = ["tasknote.darkroasted.vps-kinghost.net", "tasknoteapi.darkroasted.vps-kinghost.net"]
+      hosts       = ["tasknote.darkroasted.vps-kinghost.net", "tasknoteapi.darkroasted.vps-kinghost.net"]
       secret_name = "tasknote-tls-certs"
     }
     rule {
       host = "tasknote.darkroasted.vps-kinghost.net"
       http {
         path {
-          path = "/"
+          path      = "/"
           path_type = "Prefix"
           backend {
             service {
@@ -356,7 +355,7 @@ resource "kubernetes_ingress_v1" "tasknote_ingress" {
       host = "tasknoteapi.darkroasted.vps-kinghost.net"
       http {
         path {
-          path = "/"
+          path      = "/"
           path_type = "Prefix"
           backend {
             service {
