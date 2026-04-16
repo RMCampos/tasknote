a0432b775bd42be3cbf2899a90f05ae597548e72
Ricardo Campos <ricardompcampos@gmail.com>
2026-04-16 21:02:47 +0200

    fix: frontend test case

--- client/src/__test__/utils/UrlUtils.test.ts
+++ client/src/__test__/utils/UrlUtils.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from 'vitest';
-import { isSafeUrl } from './UrlUtils';
+import { isSafeUrl } from '../../utils/UrlUtils';
 
 describe('UrlUtils', () => {
   it('should allow http:// URLs', () => {
